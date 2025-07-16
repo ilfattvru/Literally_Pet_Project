f3a72d25576147bfd487975ae7e865bc4619b9e3
Ильфат Низамов <iinizamov@edu.hse.ru>
2025-07-16 08:14:06 +0500

    pwa builded

---
+++ app/src/main/java/io/github/ilfattvru/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.ilfattvru.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
